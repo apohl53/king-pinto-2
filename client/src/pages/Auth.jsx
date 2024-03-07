de4466f693dbd8bf114cdd4c209d50d2c4705876
alex pohlman <apohlman53@gmail.com>
2024-03-07 15:25:15 -0500

    added spacing on form

--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -68,7 +68,6 @@ function Auth({ isLogin }) {
     <>
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-         
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
             {isLogin ? "Sign in to your account" : "Register"}
           </h2>
@@ -173,17 +172,16 @@ function Auth({ isLogin }) {
                 />
               </div>
             </div>
-
-            
           </form>
           <div>
-              <button
-                type="submit"
-                className="flex w-full m-0 justify-center rounded-md bg-black px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Sign in
-              </button>
-            </div>
+            <br />
+            <button
+              type="submit"
+              className="flex w-full m-0 justify-center rounded-md bg-black px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Sign in
+            </button>
+          </div>
         </div>
       </div>
     </>
